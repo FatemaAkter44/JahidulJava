657eb2676361cfb5345cf77ffa70d3340f832238
Fatema Akter <fatemaakter@Fatemas-MBP.home>
2022-03-27 16:53:56 -0400

    initial commit

---
+++ Myapp.java
@@ -0,0 +1,10 @@
+public class Myapp{
+public static void main(String args[]){
+
+       System.out.println("Enter your first and last name");
+       System.out.println(args[0]);
+       System.out.println(args[1]);
+
+
+      }
+}
